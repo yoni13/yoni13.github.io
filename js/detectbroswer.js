3f69738662ea89b52b2f2783c72e8546c13d5974
yoni13 <76271912+yoni13@users.noreply.github.com>
2022-05-30 18:32:52 +0800

    Add files via upload

--- js/detectbroswer.js
+++ js/detectbroswer.js
@@ -19,8 +19,7 @@ if (browserName == "Opera") {
     document.getElementById("link").setAttribute("onclick", "alert('根據Mozilla Document，您的瀏覽器不支援此功能，請更換瀏覽器！')");
 }
 if (browserName == "Chrome") {
-    document.getElementById("link").setAttribute("href", "#");
-    document.getElementById("link").setAttribute("onclick", "alert('提交了，等他審核完再來看吧！')");
+    document.getElementById("link").setAttribute("href", "https://chrome.google.com/webstore/detail/%E6%9E%97%E5%A5%95%E4%BD%91%E5%96%9C%E6%AD%A1%E6%9F%90%E5%80%8B%E4%BA%BA%E4%B9%8B%E6%96%B0%E5%88%86%E9%A0%81/dhfggnpdbpngaafgomimbmknhkoahhnk?hl=zh-TW&authuser=0");
 }
 if (browserName == "MS IE") {
     document.getElementById("link").setAttribute("href", "#");
--- main.py
+++ main.py
@@ -3,4 +3,4 @@ app = Flask(__name__,static_folder='',template_folder='')
 @app.route('/')
 def root():
     return render_template('index.html')
-app.run(port = 80, debug = True)
+app.run(host = '0.0.0.0',port = 80, debug = True)
